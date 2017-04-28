a7ac66b5edb7502ac5a2b232294128c80a54c069
cvalenci <cristianvp@cristian-valencia.local>
2017-04-28 15:03:20 -0400

    edited script

--- FUT2.Step1.sh
+++ FUT2.Step1.sh
@@ -6,16 +6,17 @@
 #$ -o FUT2.Step1.out
 #$ -e FUT2.Step1.err
 ###############################
-mkdir $TMPDIR/data
+mkdir -p ~/FUT2
+cd ~/FUT2
 # download the data
-tabix -fh ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr19.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz 49199228-49209191 | temp.vcf
-# remove indels
-vcftools --vcf temp.vcf --remove-indels --recode --recode-INFO-all
-# 
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr19.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz 
+# remove indels and select region
+vcftools --gzvcf ALL.chr19.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --remove-indels --chr 19 --from-bp 49199228 --to-bp 49209191 --recode --recode-INFO-all
+# make data 
 cat out.recode.vcf| grep -v '##' | cut -f1,2,3,4,5,7,8| awk  '{OFS="\t";  split($7,x,";"); print $1,$2,$3,$4,$5,$6,x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10]}' > fut2.data.txt
 # data file called: fut2.data.txt
-# copy data to dir
-cp fut2.data.txt /home/cvalenc1/FUT2_3_TLR5/
+# remove files:
+rm ALL.chr19.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz out.recode.vcf
 ############################
 exit
 
